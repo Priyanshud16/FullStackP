0e18259d204da5ea7db153f653413e7f85b0f4ed
Priyanshud16 <priyanshuharrdy@gmail.com>
2024-04-29 11:56:02 +0530

    Added

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,6 @@
 const {Router}=require("express")
 const bcrypt=require("bcrypt")
+const dotenv=require("dotenv").config()
 const userModel = require("../model/userModel")
 const jwt=require("jsonwebtoken")
 const userRouter=Router()
@@ -33,7 +34,7 @@ userRouter.post('/login',async(req,res)=>{
       bcrypt.compare(password, user.password, async(err, result)=> {
         // result == true
         if(result){
-          const token = jwt.sign({ userID:user._id,user:user.username }, 'masai' );
+          const token = jwt.sign({ userID:user._id,user:user.username }, process.env.JWT_SECRET );
           res.status(200).json({message:"User Login Successfully",token})
         }else{
           res.status(401).json({message:"Wrong Password"})
